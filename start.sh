d5a0ac04cc7eac70b628122b3a6f16ea8a08b8ff
Sharif Mohammad Nasrullah <sharif418@users.noreply.github.com>
2026-06-28 13:59:02 +0600

    fix: add db push back to start script

--- start.sh
+++ start.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
-
-# Push database schema (safely creates tables if they don't exist)
-# Using direct node call to the prisma CLI binary we copied
-echo "Running Prisma migrations..."
 node node_modules/prisma/build/index.js db push --accept-data-loss
-
-echo "Starting Next.js server..."
 node_modules/.bin/next start -p 3000
